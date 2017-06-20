d1eee97fce4af3b35d48edd7fa20aa08714799e5
alexax66 <alexax66@mail.ru>
2017-06-20 22:36:19 +0000

    build_kernel.sh: add switching between a3xelte and a5xelte variants

--- build_kernel.sh
+++ build_kernel.sh
@@ -8,7 +8,29 @@ if [ ! -d $(pwd)/output ];
         mkdir $(pwd)/output;
     fi
 
-make -C $(pwd) O=output ARCH=arm64 a3xelte_00_defconfig
+case "$1" in
+        a3)
+            VARIANT="a3xelte"
+            ;;
+
+        a5)
+            VARIANT="a5xelte"
+            ;;
+
+        *)
+            VARIANT="a3xelte"
+esac
+
+
+if [ ! -d $(pwd)/output ];
+    then
+        mkdir $(pwd)/output;
+    fi
+
+make -C $(pwd) O=output ARCH=arm64 "alexax_"$VARIANT"_defconfig"
+
+#make -C $(pwd) O=output ARCH=arm64 alexax_a3xelte_defconfig
+
 make -j6 -C $(pwd) O=output ARCH=arm64
 
 $(pwd)/dtbtool  -o $(pwd)/output/arch/arm64/boot/dt.img -s 2048 -p $(pwd)/output/scripts/dtc/ $(pwd)/output/arch/arm64/boot/dts/ -v
